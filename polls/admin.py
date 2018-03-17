7617697a6980de63bb678ecaedebe9614c0c4365
hildyale <hildyale@gmail.com>
2018-03-17 12:09:00 -0500

    final

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Question, Choice
 from django.contrib import admin
+
+admin.site.register(Question)
+admin.site.register(Choice)
